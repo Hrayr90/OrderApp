adaa268a27cb49a336549f012a8ed0ec2a78c442
Hrayr Hakobyan <hrayr.hakobyan1990@gmail.com>
2022-02-03 22:32:14 +0400

    no message

---
+++ OrderApp/Model/Order.swift
@@ -0,0 +1,16 @@
+//
+//  Order.swift
+//  OrderApp
+//
+//  Created by Hrayr  Hakobyan on 31.01.22.
+//
+
+import Foundation
+
+struct Order: Codable {
+    var menuItems: [MenuItem]
+    
+    init(menuItems: [MenuItem] = []) {
+        self.menuItems = menuItems
+    }
+}
