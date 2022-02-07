b21ae3dd06810f807673fed9ad3c932a9e9f71fd
Hrayr Hakobyan <hrayr.hakobyan1990@gmail.com>
2022-02-07 14:39:40 +0400

    fix partThree

--- OrderApp/Controller/MenuItemDetaliViewController.swift
+++ OrderApp/Controller/MenuItemDetaliViewController.swift
@@ -12,7 +12,6 @@ class MenuItemDetaliViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
