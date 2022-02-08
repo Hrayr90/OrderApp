bbeb094297153b32dd5646b7ea1d3a8c0834546b
Hrayr Hakobyan <hrayr.hakobyan1990@gmail.com>
2022-02-08 19:36:10 +0400

    Parse the Responses

--- OrderApp/Controller/MenuController.swift
+++ OrderApp/Controller/MenuController.swift
@@ -8,34 +8,73 @@
 import Foundation
 
 class MenuController {
-    let baseURL = URL(string: "http://localhost:8080/")
+    private let baseURL = URL(string: "http://localhost:8080/")
     
     func fetchCategories() async throws -> [String] {
-        let categoriesURL = baseURL?.appendingPathComponent("categories")
-        let (dat, response) = try await URLSession.shared.data(from: categoriesURL)
+        guard let categoriesURL = baseURL?.appendingPathComponent("categories") else { fatalError() }
+        
+        let (data, response) = try await URLSession.shared.data(from: categoriesURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200
+        else {
+            throw MenuControllerError.categoriesNotFound
+        }
+        
+        let decoder = JSONDecoder()
+        let categoriesResponse = try decoder.decode(CategoriesResponse.self, from: data)
+        
+        return categoriesResponse.categories
     }
     
     func fetchMenuItams(forCategary categoryName: String) async throws -> [MenuItem] {
-        let baseMenuURL = baseURL?.appendingPathComponent("menu")
+        
+        guard let baseMenuURL = baseURL?.appendingPathComponent("menu") else { fatalError() }
+        
         var components = URLComponents(url: baseMenuURL, resolvingAgainstBaseURL: true)!
+        
         components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        
         let menuURL = components.url!
         let (data, response) = try await URLSession.shared.data(from: menuURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200
+        else {
+            throw MenuControllerError.menuItemsNotFound
+        }
+        
+        let decoder = JSONDecoder()
+        let menuResponse = try decoder.decode(MenuResponse.self, from: data)
+        
+        return menuResponse.items
     }
     
     typealias MinutesToPrepare = Int
     
     func submitOrder(forMenuIDs menuIDs: [Int]) async throws -> MinutesToPrepare {
-        let orderURL = baseURL?.appendingPathComponent("order")
+        guard let orderURL = baseURL?.appendingPathComponent("order") else { fatalError() }
+        
         var request = URLRequest(url: orderURL)
+        
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
         let menuIdsDict = ["menuIds": menuIDs]
         let jsonEncoder = JSONEncoder()
         let jsonData = try? jsonEncoder.encode(menuIdsDict)
-        request.httpBody = jsonEncoder
         
-        let (data, response) = try await URLSession.shared.data(from: request)
+        request.httpBody = jsonData
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200
+        else {
+            throw MenuControllerError.orderRequestFailed
+        }
+        
+        let decoder = JSONDecoder()
+        let orderResponse = try decoder.decode(OrderResponse.self, from: data)
+        
+        return orderResponse.prepTime
     }
 }
 
