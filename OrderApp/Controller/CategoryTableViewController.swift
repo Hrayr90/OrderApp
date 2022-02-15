f6afac82f0525b7ef6efaa5918519c3042192989
Hrayr Hakobyan <hrayr.hakobyan1990@gmail.com>
2022-02-15 21:06:47 +0400

    Basic Table Setup

--- OrderApp/Controller/CategoryTableViewController.swift
+++ OrderApp/Controller/CategoryTableViewController.swift
@@ -8,77 +8,58 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
-
+    let menuController = MenuController()
+    var categories = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        Task.init {
+            do {
+                let categaries = try await menuController.fetchCategories()
+                updateUI(with: categaries)
+            } catch {
+                displayError(error, title: "Failed to Fetch Categories")
+            }
+        }
     }
 
+    func updateUI(with categories: [String]) {
+        self.categories = categories
+        self.tableView.reloadData()
+    }
+    
+    func displayError(_ error: Error, title: String){
+        guard let _ = viewIfLoaded?.window else { return }
+        
+        let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        self.present(alert, animated: true)
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Category", for: indexPath)
 
-        // Configure the cell...
-
+        configureCell(cell, forCategoryAt: indexPath)
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func configureCell(_ cell: UITableViewCell, forCategoryAt indexPath: IndexPath) {
+        let categoty = categories[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = categoty.capitalized
+        cell.contentConfiguration = content
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
