b5220656022a1a22132f8d574b38f132fea2d29c
Hrayr Hakobyan <hrayr.hakobyan1990@gmail.com>
2022-02-17 15:16:01 +0400

    Menu Basic Table Setup

--- OrderApp/Controller/MenuTableViewController.swift
+++ OrderApp/Controller/MenuTableViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class MenuTableViewController: UITableViewController {
 
     let category: String
-    
+    let menuController = MenuController()
+    var menuItems = [MenuItem]()
     init?(coder: NSCoder, category: String) {
         self.category = category
         super.init(coder: coder)
@@ -18,5 +19,60 @@ class MenuTableViewController: UITableViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = category.capitalized
+        
+        Task.init {
+            do {
+                let menuItems = try await menuController.fetchMenuItams(forCategary: category)
+                updateUI(with: menuItems)
+            } catch {
+                displayError(error, title: "Failed to Fetch Menu Items for \(self.category)")
+            }
+        }
+    }
+    
+    func updateUI(with menuItems: [MenuItem]) {
+        self.menuItems = menuItems
+        self.tableView.reloadData()
+    }
+    
+    func displayError(_ error:Error, title: String) {
+        guard let _ = viewIfLoaded?.window else { return }
+        
+        let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        self.present(alert, animated: true)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItem", for: indexPath)
+        configure(cell, forIteamAt: indexPath)
+        return cell
+    }
+    
+    func configure(_ cell: UITableViewCell,forIteamAt indexPath: IndexPath) {
+        let menuItem = menuItems[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = menuItem.name
+        content.secondaryText = menuItem.price.formatted(.currency(code: "usd"))
+        cell.contentConfiguration = content
     }
 }
