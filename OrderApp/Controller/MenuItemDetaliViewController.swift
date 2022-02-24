676718d46437e2d0ea818f2043df952bf2f9c801
Hrayr Hakobyan <hrayr.hakobyan1990@gmail.com>
2022-02-24 19:06:08 +0400

    Customize the Button

--- OrderApp/Controller/MenuItemDetaliViewController.swift
+++ OrderApp/Controller/MenuItemDetaliViewController.swift
@@ -15,6 +15,20 @@ class MenuItemDetaliViewController: UIViewController {
     @IBOutlet weak var detaliTextLable: UILabel!
     @IBOutlet weak var addToOrderButton: UIButton!
     
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0.1,
+            options: [],
+            animations:{
+                self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+                self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
